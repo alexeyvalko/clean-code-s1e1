2409d671fdec00f920134269eae64ee7e39aa06e
alexeyvalko <valko.alexey@gmail.com>
2021-03-30 20:28:20 +0300

    fix: replace tag selectors according to rule 3.4 css-and-html

--- app.js
+++ app.js
@@ -33,16 +33,18 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
-
+    editInput.className="task task-input";
+    
+    deleteButtonImg.className = "delete-img";
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
-
+    listItem.className="task-list-element"
+    checkBox.className="checkbox"
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
